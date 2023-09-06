8660d4b488f45af3b25c497350e7d28d4677e549
Sergey Zavyalov <ser500vms@gmail.com>
2023-09-06 17:31:39 +0300

    Done task_11

--- 1/Task_1.py
+++ 1/Task_1.py
@@ -1,6 +1,7 @@
 # По данному целому неотрицательному n вычислите значение n!
 # решаем с while
 
+# Вариант 1
 n = int(input("n = "))
 count = 1
 factorial = 1
@@ -8,3 +9,11 @@ while count <= n:
     factorial *= count
     count += 1
 print(f'{n}! = {factorial}') 
+
+# Вариант 2
+n = int(input("n = "))
+factorial = 1
+while n > 1:
+    factorial *= n
+    n -= 1
+print(f'{n}! = {factorial}')
